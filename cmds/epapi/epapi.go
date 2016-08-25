f272a5e44141995a67ad0171289a284d47f9d9b1
R. S. Doiel <rsdoiel@gmail.com>
2016-08-25 16:37:54 -0700

    Quick save

---
+++ cmds/epapi/epapi.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("epapi not implemented.")
+	os.Exit(1)
+}
---
+++ cmds/epindexer/epindexer.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("epindexer not implemented.")
+	os.Exit(1)
+}
