f2c94b43a8edb8d98c7da17eb45bc6b37e73f9ee
R. S. Doiel <rsdoiel@gmail.com>
2016-12-08 12:46:10 -0800

    Quick save

--- epgo.go
+++ epgo.go
@@ -45,7 +45,7 @@ import (
 
 const (
 	// Version is the revision number for this implementation of epgo
-	Version = "v0.0.8"
+	Version = "v0.0.9"
 
 	// Ascending sorts from lowest (oldest) to highest (newest)
 	Ascending = iota
