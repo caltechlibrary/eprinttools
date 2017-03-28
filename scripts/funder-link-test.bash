cb834811e0cbbd5d911a8f628f00e4d88e7ab124
R. S. Doiel <rsdoiel@gmail.com>
2017-03-28 13:40:22 -0700

    linted and formatted Bash scripts

--- scripts/funder-link-test.bash
+++ scripts/funder-link-test.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-finddir -depth 3 htdocs/funder | while read TITLE; do 
-    LINK=$(urlencode $TITLE)
-    echo "+ [$TITLE]($LINK)"; 
-done > htdocs/funder/index.md
+finddir -depth 3 htdocs/funder | while read TITLE; do
+	LINK=$(urlencode "$TITLE")
+	echo "+ [$TITLE]($LINK)"
+done >htdocs/funder/index.md
 mkpage \
-    "content=htdocs/funder/index.md" \
-    templates/default/index.html \
-    > htdocs/funder/index.html
+	"content=htdocs/funder/index.md" \
+	templates/default/index.html \
+	>htdocs/funder/index.html
--- scripts/harvest-orcid-profiles.bash
+++ scripts/harvest-orcid-profiles.bash
@@ -4,27 +4,26 @@
 # Harvest the ORCID profiles via the ORCID API
 #
 function HarvestProfiles() {
-  template=$1
-  profilePath=$2
-  for ITEM in $(find $profilePath -depth 1 -type d | sed -E 's/\// /g' | cut -d\  -f 3); do
-    echo "Building orcid profile for $ITEM"
-    orcid -profile $ITEM > $profilePath/$ITEM/orcid-profile.json
-    # FIXME: I need to pickup the template set from the environment
-    mkpage ".=$profilePath/$ITEM/orcid-profile.json" $template > $profilePath/$ITEM/index.html
-  done
+	template="$1"
+	profilePath="$2"
+	for ITEM in $(find "$profilePath" -depth 1 -type d | sed -E 's/\// /g' | cut -d\  -f 3); do
+		echo "Building orcid profile for $ITEM"
+		orcid -profile "$ITEM" >"$profilePath/$ITEM/orcid-profile.json"
+		# FIXME: I need to pickup the template set from the environment
+		mkpage ".=$profilePath/$ITEM/orcid-profile.json" "$template" >"$profilePath/$ITEM/index.html"
+	done
 }
 
 #
 # Check to make sure all the environment vars needed are defined before running harvest
 #
 function CheckEnv() {
-    VarName=$1
-    if [ "$2" = "" ]; then
-        echo "Missing $VarName"
-        exit 1
-    fi
+	VarName="$1"
+	if [ "$2" = "" ]; then
+		echo "Missing $VarName"
+		exit 1
+	fi
 }
-
 
 #
 # Main processing
@@ -33,12 +32,12 @@ CheckEnv "ORCID_API_URL" "$ORCID_API_URL"
 CheckEnv "ORCID_CLIENT_ID" "$ORCID_CLIENT_ID"
 CheckEnv "ORCID_CLIENT_SECRET" "$ORCID_CLIENT_SECRET"
 if [ ! -d htdocs/person ]; then
-    echo "Can't find htdocs/person"
-    exit 1
+	echo "Can't find htdocs/person"
+	exit 1
 fi
 # FIXME: Need to check to make sure template is available too and set from the environment 
 if [ ! -f templates/default/orcid-profile.tmpl ]; then
-    echo "Can't find templates/default/orcid-profile.tmpl"
-    exit 1
+	echo "Can't find templates/default/orcid-profile.tmpl"
+	exit 1
 fi
 HarvestProfiles templates/default/orcid-profile.tmpl htdocs/person
