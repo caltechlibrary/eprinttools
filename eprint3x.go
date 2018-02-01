80dfb64f032b825f0478aa60fc130a3636b17096
R. S. Doiel <rsdoiel@gmail.com>
2018-01-31 17:18:24 -0800

    Quick Save

--- eprint3x.go
+++ eprint3x.go
@@ -32,7 +32,7 @@ import (
 // E.g. curl -L -O https://eprints3.example.org/rest/eprint/1234.xml
 // Then parse the 1234.xml document stucture.
 type EPrints struct {
-	XMLName xml.Name `json:"-"`
+	XMLName xml.Name `xml:"eprints" json:"-"`
 	XMLNS   string   `xml:"xmlns,attr,omitempty" json:"xmlns,omitempty"`
 	EPrint  *EPrint  `xml:"eprint" json:"eprint"`
 }
