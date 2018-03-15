b442790a11a50415bb02bd6dcb50eb56c43fd750
R. S. Doiel <rsdoiel@gmail.com>
2018-03-15 08:29:20 -0700

    prep for v0.0.15 release, improved pymodule

--- eprinttools.go
+++ eprinttools.go
@@ -39,7 +39,7 @@ import (
 
 const (
 	// Version is the revision number for this implementation of epgo
-	Version = `v0.0.14`
+	Version = `v0.0.15`
 
 	// LicenseText holds the string for rendering License info on the command line
 	LicenseText = `
