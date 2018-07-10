02c3ab860dfbcb144c1993a8237c7e8c7096c380
R. S. Doiel <=>
2018-07-10 14:41:20 -0700

    getting mapping function working

--- crossref.go
+++ crossref.go
@@ -31,10 +31,44 @@ func indexInto(m map[string]interface{}, parts ...string) (interface{}, bool) {
 	}
 }
 
+// normalizeLocalGroup takes an affiliation from CrossRef author
+// and attempts to determine if it is a Caltech Group and returns
+// the normalized name or an empty string.
+func normalizeLocalGroup(s string) string {
+	s = strings.ToLower(s)
+	switch {
+	case strings.Contains(s, "california institute of technology"):
+		i := strings.Index(s, "california institute of technology")
+		if i > -1 {
+			s = strings.TrimSuffix(strings.TrimSpace(s[0:i-1]), ";")
+		}
+		return strings.TrimSpace(s)
+	case strings.Contains(s, "california institution of technology"):
+		i := strings.Index(s, "california institution of technology")
+		if i > -1 {
+			s = strings.TrimSuffix(strings.TrimSpace(s[0:i-1]), ";")
+		}
+		return strings.TrimSpace(s)
+	case strings.Contains(s, "caltech"):
+	case strings.Contains(s, "caltech"):
+		i := strings.Index(s, "caltech")
+		if i > -1 {
+			s = strings.TrimSuffix(strings.TrimSpace(s[0:i-1]), ";")
+		}
+		return strings.TrimSpace(s)
+	case strings.Contains(s, "ligo"):
+		return "LIGO"
+	}
+	return ""
+}
+
 // normalizeType converts content type from CrossRef to Authors (e.g.
 // "journal-article" to "article"
 func normalizeType(s string) string {
 	switch strings.ToLower(s) {
+	case "proceedings-article":
+		//NOTE: This seems vary idiosyncratic to CaltechAUTHORS
+		return "book_section"
 	case "journal-article":
 		return "article"
 	default:
@@ -59,8 +93,6 @@ func CrossRefWorksToEPrint(obj crossrefapi.Object) (*EPrint, error) {
 			eprint.Title = fmt.Sprintf("%s", a.([]interface{})[0].(string))
 		}
 	}
-	// ShortTitle
-	// SubTitle
 
 	// NOTE: Assuming IsPublished is true given that we're talking to
 	// CrossRef API which holds published content.
@@ -72,51 +104,60 @@ func CrossRefWorksToEPrint(obj crossrefapi.Object) (*EPrint, error) {
 		eprint.Publisher = fmt.Sprintf("%s", s)
 	}
 	// Publication
-	if l, ok := indexInto(obj, "message", "container-title"); ok == true {
-		if len(l.([]interface{})) > 0 {
-			eprint.Publication = l.([]interface{})[0].(string)
+	if eprint.Type == "article" {
+		if l, ok := indexInto(obj, "message", "container-title"); ok == true {
+			if len(l.([]interface{})) > 0 {
+				eprint.Publication = l.([]interface{})[0].(string)
+			}
 		}
 	}
 	// Series
-	//FIXME: Need to find value in CrossRef works metadata for this
+	if eprint.Type == "book" {
+		if l, ok := indexInto(obj, "message", "container-title"); ok == true {
+			if len(l.([]interface{})) > 0 {
+				eprint.Series = l.([]interface{})[0].(string)
+			}
+		}
+	}
+	if l, ok := indexInto(obj, "message", "short-container-title"); ok == true {
+		if len(l.([]interface{})) > 0 {
+			eprint.Series = l.([]interface{})[0].(string)
+		}
+	}
 
 	// Volume
-	if s, ok := indexInto(obj, "message", "volume"); ok == true {
-		eprint.Volume = fmt.Sprintf("%s", s)
+	if eprint.Type == "article" {
+		if s, ok := indexInto(obj, "message", "volume"); ok == true {
+			eprint.Volume = fmt.Sprintf("%s", s)
+		}
+		// Number
+		if s, ok := indexInto(obj, "message", "journal-issue", "issue"); ok == true {
+			eprint.Number = fmt.Sprintf("%s", s)
+		}
+
 	}
-	// Number
-	//FIXME: Need to find value in CrossRef works metadata for this
 
-	// Edition
-	//FIXME: Need to find value in CrossRef works metadata for this
-
-	// PlaceOfPub
-	//FIXME: Need to find value in CrossRef works metadata for this
+	// PlaceOfPub taken from publisher-location in CrossRef
+	if s, ok := indexInto(obj, "message", "publisher-location"); ok == true {
+		eprint.PlaceOfPub = fmt.Sprintf("%s", s)
+	}
 
 	// PageRange
-	//FIXME: Need to find value in CrossRef works metadata for this
-
 	// Pages
-	//FIXME: Need to find value in CrossRef works metadata for this
-
-	// FullTextStatus
-	//FIXME: Need to find value in CrossRef works metadata for this
-
-	// Keywords
-	//FIXME: Need to find value in CrossRef works metadata for this
-
-	// Note
-	//FIXME: Need to find value in CrossRef works metadata for this
-
-	// Abstract
-	//FIXME: Need to find value in CrossRef works metadata for this
-
-	// Refereed
-	//FIXME: Need to find value in CrossRef works metadata for this
+	if s, ok := indexInto(obj, "message", "page"); ok == true {
+		if strings.Contains(s.(string), "-") {
+			eprint.PageRange = fmt.Sprintf("%s", s)
+		} else {
+			eprint.Pages = fmt.Sprintf("%s", s)
+		}
+	}
 
 	// ISBN
-	if s, ok := indexInto(obj, "message", "ISBN"); ok == true {
-		eprint.ISSN = fmt.Sprintf("%s", s)
+	if a, ok := indexInto(obj, "message", "ISBN"); ok == true {
+		if len(a.([]interface{})) > 0 {
+			s := a.([]interface{})[0]
+			eprint.ISBN = fmt.Sprintf("%s", s)
+		}
 	}
 
 	// ISSN
@@ -126,29 +167,29 @@ func CrossRefWorksToEPrint(obj crossrefapi.Object) (*EPrint, error) {
 		}
 	}
 
+	// NOTE: This doesn't appear to be used by CaltechAUTHORS for full book
 	// BookTitle
-	//FIXME: Need to find value in CrossRef works metadata for this
-
-	// Editors (*EditorItemList)
-	//FIXME: Need to find value in CrossRef works metadata for this
-
-	// Projects
-	//FIXME: Need to find value in CrossRef works metadata for this
+	if eprint.Title != "" && eprint.Type == "book" {
+		eprint.BookTitle = eprint.Title
+	}
 
 	// Funders
-	//FIXME: Need to find value in CrossRef works metadata for this
-
-	// Contributors (*ContriborItemList)
-	//FIXME: Need to find value in CrossRef works metadata for this
-
-	// MonographType???
-	//FIXME: Need to find value in CrossRef works metadata for this
-
-	// Subjects???
-	//FIXME: Need to find value in CrossRef works metadata for this
-
-	// PresType (presentation type)???
-	//FIXME: Need to find value in CrossRef works metadata for this
+	if a, ok := indexInto(obj, "message", "funder"); ok == true {
+		eprint.Funders = new(FunderItemList)
+		for _, item := range a.([]interface{}) {
+			entry := new(Item)
+			m := item.(map[string]interface{})
+			if name, ok := indexInto(m, "name"); ok == true {
+				entry.Agency = fmt.Sprintf("%s", name)
+			}
+			if a2, ok := indexInto(m, "award"); ok == true {
+				if len(a2.([]interface{})) > 0 {
+					entry.GrantNumber = fmt.Sprintf("%s", a2.([]interface{})[0])
+				}
+			}
+			eprint.Funders.AddItem(entry)
+		}
+	}
 
 	// NOTE: Caltech Library puts the DOI in a different field than
 	// EPrints' standard DOI location
@@ -214,10 +255,22 @@ func CrossRefWorksToEPrint(obj crossrefapi.Object) (*EPrint, error) {
 	if l, ok := indexInto(obj, "message", "author"); ok == true {
 		creators := new(CreatorItemList)
 		corpCreators := new(CorpCreatorItemList)
+		localGroups := new(LocalGroupItemList)
 		for _, entry := range l.([]interface{}) {
 			author := entry.(map[string]interface{})
 			item := new(Item)
 			item.Name = new(Name)
+			if affiliations, ok := author["affiliation"]; ok == true {
+				for _, affiliation := range affiliations.([]interface{}) {
+					if s, ok := indexInto(affiliation.(map[string]interface{}), "name"); ok == true {
+						group := new(Item)
+						group.Value = normalizeLocalGroup(s.(string))
+						if group.Value != "" {
+							localGroups.AddItem(group)
+						}
+					}
+				}
+			}
 			if orcid, ok := author["ORCID"]; ok == true {
 				item.ORCID = orcid.(string)
 				if strings.HasPrefix(orcid.(string), "http://orcid.org/") {
@@ -255,5 +308,42 @@ func CrossRefWorksToEPrint(obj crossrefapi.Object) (*EPrint, error) {
 			eprint.CorpCreators = corpCreators
 		}
 	}
+
+	// Edition
+	//FIXME: Need to find value in CrossRef works metadata for this
+
+	// FullTextStatus
+	//FIXME: Need to find value in CrossRef works metadata for this
+
+	// Keywords
+	//FIXME: Need to find value in CrossRef works metadata for this
+
+	// Note
+	//FIXME: Need to find value in CrossRef works metadata for this
+
+	// Abstract
+	//FIXME: Need to find value in CrossRef works metadata for this
+
+	// Refereed
+	//FIXME: Need to find value in CrossRef works metadata for this
+
+	// Editors (*EditorItemList)
+	//FIXME: Need to find value in CrossRef works metadata for this
+
+	// Projects
+	//FIXME: Need to find value in CrossRef works metadata for this
+
+	// Contributors (*ContriborItemList)
+	//FIXME: Need to find value in CrossRef works metadata for this
+
+	// MonographType
+	//FIXME: Need to find value in CrossRef works metadata for this
+
+	// Subjects
+	//FIXME: Need to find value in CrossRef works metadata for this
+
+	// PresType (presentation type)???
+	//FIXME: Need to find value in CrossRef works metadata for this
+
 	return eprint, nil
 }
