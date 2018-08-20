f2f432ff7addd264fe7eadd035cf5ebff90a2029
R. S. Doiel <rsdoiel@gmail.com>
2018-08-20 12:47:45 -0700

    bumped version number

--- eprinttools.go
+++ eprinttools.go
@@ -35,7 +35,7 @@ import (
 
 const (
 	// Version is the revision number for this implementation of epgo
-	Version = `v0.0.21`
+	Version = `v0.0.22`
 
 	// LicenseText holds the string for rendering License info on the command line
 	LicenseText = `
