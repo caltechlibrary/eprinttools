da416addd9dc2b0236dbabd655595febc4e30e35
R. S. Doiel <rsdoiel@gmail.com>
2019-04-26 18:28:05 -0700

    Quick Save

--- eprinttools.go
+++ eprinttools.go
@@ -35,7 +35,7 @@ import (
 
 const (
 	// Version is the revision number for this implementation of epgo
-	Version = `v0.0.43*2`
+	Version = `v0.0.43`
 
 	// LicenseText holds the string for rendering License info on the command line
 	LicenseText = `
