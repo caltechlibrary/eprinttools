7cdccd1e6b3d6b6768eb81bb2d2307844f572935
R. S. Doiel <rsdoiel@gmail.com>
2020-01-07 10:28:52 -0800

    Version v0.0.56 updates

--- eprinttools.go
+++ eprinttools.go
@@ -35,7 +35,7 @@ import (
 
 const (
 	// Version is the revision number for this implementation of epgo
-	Version = `v0.0.55`
+	Version = `v0.0.56`
 
 	// LicenseText holds the string for rendering License info on the command line
 	LicenseText = `
