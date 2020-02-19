2fc9da3b6d3faca49386e11bfed29708ad0195cd
R. S. Doiel <rsdoiel@gmail.com>
2020-02-19 11:32:30 -0800

    prep for v0.0.58 release

--- eprinttools.go
+++ eprinttools.go
@@ -35,7 +35,7 @@ import (
 
 const (
 	// Version is the revision number for this implementation of epgo
-	Version = `v0.0.56`
+	Version = `v0.0.57`
 
 	// LicenseText holds the string for rendering License info on the command line
 	LicenseText = `
