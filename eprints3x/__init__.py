e0fd2acb03f7421086cec862a15736a1a0ec0508
R. S. Doiel <rsdoiel@gmail.com>
2020-08-11 13:41:35 -0700

    improved eprints3x Python module, added support for recently modified key filter using REST API

--- eprints3x/__init__.py
+++ eprints3x/__init__.py
@@ -1,1 +1,1 @@
-from .eprints import harvest_init, harvest, harvest_keys, harvest_record, skip_and_prune
+from .eprints import harvest_init, harvest, harvest_keys, harvest_record, skip_and_prune, filter_recently_modified_keys
