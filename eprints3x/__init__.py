1417b7158e47d1d11f9b6bf641c804296257509d
Robert Doiel <rsdoiel@gmail.com>
2020-08-14 12:39:13 -0700

    Added support for publishing to AWS S3 buckets if AWS cli are installed, added more configurability to the eprints3x module.

--- eprints3x/__init__.py
+++ eprints3x/__init__.py
@@ -1,1 +1,6 @@
-from .eprints import harvest_init, harvest, harvest_keys, harvest_record, skip_and_prune, filter_recently_modified_keys
+from .eprints import harvest_init, harvest, harvest_keys, harvest_record, skip_and_prune
+from .subjects import load_subjects, normalize_subject
+from .views import load_views, normalize_view, has_view, supported_views
+from .users import load_users, normalize_user, has_user, get_user, user_list
+from .logger import Logger
+from .s3_publisher import s3_publish
