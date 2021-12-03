afcb201901f4470c07ce5517912b0712d6e39f94
Robert Doiel <rsdoiel@gmail.com>
2021-12-02 16:47:39 -0800

    Improved test cases for cleaner

--- cleaner/cleaner_test.go
+++ cleaner/cleaner_test.go
@@ -87,4 +87,13 @@ This text has <span id="3" class="wicked-thing">encoded elements</span>. cleaner
 	}
 	got = StripTags(src)
 	assertBytes(t, expected, got, `expected %q, got %q, cleaner.StripTags(src): %s`, expected, got, src)
+
+	src = []byte(`<jats:title>Abstract</jats:title><jats:p>C1q/TNF-related protein 1 (CTRP1) is a CTRP family member that has collagenous and globular C1q-like domains. The secreted form of CTRP1 is known to be associated with cardiovascular and metabolic diseases, but its cellular roles have not yet been elucidated. Here, we showed that cytosolic CTRP1 localizes to the endoplasmic reticulum (ER) membrane and that knockout or depletion of CTRP1 leads to mitochondrial fission defects, as demonstrated by mitochondrial elongation. Mitochondrial fission events are known to occur through an interaction between mitochondria and the ER, but we do not know whether the ER and/or its associated proteins participate directly in the entire mitochondrial fission event. Interestingly, we herein showed that ablation of CTRP1 suppresses the recruitment of DRP1 to mitochondria and provided evidence suggesting that the ER–mitochondrion interaction is required for the proper regulation of mitochondrial morphology. We further report that CTRP1 inactivation-induced mitochondrial fission defects induce apoptotic resistance and neuronal degeneration, which are also associated with ablation of DRP1. These results demonstrate for the first time that cytosolic CTRP1 is an ER transmembrane protein that acts as a key regulator of mitochondrial fission, providing new insight into the etiology of metabolic and neurodegenerative disorders.</jats:p>`)
+
+	expected = src[:]
+	for _, target := range []string{`<jats:title>`, `</jats:title>`, `<jats:p>`, `</jats:p>`} {
+		expected = bytes.ReplaceAll(expected, []byte(target), []byte(``))
+	}
+	got = StripTags(src)
+	assertBytes(t, expected, got, `expected %q, got %q, cleaner.StripTags(src): %s`, expected, got, src)
 }
