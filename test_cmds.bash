3c05b6571b53d6b87819d6628334458f14918f59
Robert Doiel <rsdoiel@gmail.com>
2021-12-06 16:44:17 -0800

    fixed typo in test scripts

--- test_cmds.bash
+++ test_cmds.bash
@@ -84,7 +84,7 @@ echo 'Testing ep3apid'
 if [[ -f "test-settings.json" && -f "bin/ep3apid" ]]; then
    echo 'Resetting lemurprints database'
    mysql lemurprints < srctest/lemurprints-setup-schema.sql
-   echo 'Starting epi3apid and waiting 20 seconds'
+   echo 'Starting ep3apid and waiting 20 seconds'
    ./bin/ep3apid test-settings.json &
    PID=$!
    # Add content to empty repository database
