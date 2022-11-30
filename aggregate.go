7aab56be2b7c44fd650c521cbbde0821b62bfdb2
R. S. Doiel <rsdoiel@gmail.com>
2022-11-29 17:06:21 -0800

    feat: stubbing out aggregation toool ep3aggregator

---
+++ aggregate.go
@@ -0,0 +1,8 @@
+package eprinttools
+
+/*
+This file should define and orchestrate aggregating EPrint content
+into "view" (e.g. by record type, by pub date, by author, by editor, etc).
+It needs to work across all our repositories and be general enough for
+an easy fit when we've migrated to a new repository system.
+ */
---
+++ aggregate_test.go
@@ -0,0 +1,19 @@
+package eprinttools
+
+/*
+This file should test our definitions and orchestratation for 
+aggregating EPrint content into "views" (e.g. by record type, 
+by pub date, by author, by editor, etc).
+It needs to work across all our repositories and be general enough for
+an easy fit when we've migrated to a new repository system.
+ */
+
+ import (
+ "testing"
+ )
+
+ func TestAggregation(t *testing.T) {
+	 t.Errorf("Aggregation not implemented")
+	 t.Errorf("TestAggregation not implemented")
+ }
+
