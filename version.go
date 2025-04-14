81ac9884c23fb9d2bcdf295ea5494f454e66aeab
R. S. Doiel <rsdoiel@gmail>
2025-04-14 16:06:27 -0700

    commiting to main

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2025-04-14"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "d2db69e"
+    ReleaseHash = "4a92c91"
     LicenseText = `
 
 Copyright (c) 2022, Caltech
